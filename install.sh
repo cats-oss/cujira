aba40c1e56c32a5eee8239c57c9bc502b930e5bf
marty-suzuki <s1180183@gmail.com>
2018-06-13 14:18:52 +0900

    fix sh

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 USR_LOCAL_DIR="/usr/local"
 USR_LOCAL_BIN_DIR="${USR_LOCAL_DIR}/bin"
+CMD_NAME="cujira"
 
 PROJ_DIR="$(cd $(dirname $0) && pwd)"
 
 swift build -c release
-cp "${PROJ_DIR}/.build/release/cujira" "${USR_LOCAL_BIN_DIR}/cujira"
+cp "${PROJ_DIR}/.build/release/${CMD_NAME}" "${USR_LOCAL_BIN_DIR}/${CMD_NAME}"
